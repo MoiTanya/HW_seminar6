bd4a673ba95013b747bcb6c91c3d32bcb5103a69
MoiTanya <130609745+MoiTanya@users.noreply.github.com>
2023-05-30 23:11:53 +0300

    Add files via upload
    task41 and task43 commit

---
+++ task41.cs
@@ -0,0 +1,15 @@
+﻿// Пользователь вводит с клавиатуры М чисел. Посчитайте сколько чисел больше 0 ввел пользователь
+// 0, 7, 8, -2, -2 -> 2
+// 1,-7, -567, 89, 223 -> 3
+
+Console.WriteLine("Введите числа через запятую");
+string Temp = Console.ReadLine();
+string[] array = Temp.Split(',');
+int[] intArray = new int[array.Length];
+int count = 0;
+for (int i=0; i< array.Length; i++)
+{
+    intArray[i] = Convert.ToInt32(array[i]);
+    if (intArray[i]>0) count++;
+}
+Console.WriteLine($"{Temp} -> {count}");
